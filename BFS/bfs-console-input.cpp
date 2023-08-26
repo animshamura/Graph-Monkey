49fcff39a1f595003d69e6ac7a7f6631d0755747
Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-08-26 23:59:04 +0600

    Create bfs-console-input.cpp

---
+++ BFS/bfs-console-input.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e5;
+
+bool vis[mx];
+vector <int> graph[mx];
+
+void bfs(int u) {
+    queue <int> q;
+    q.push(u);
+    vis[u] = true;
+
+    while (!q.empty()) {
+        u = q.front();
+        q.pop();
+        cout << u << " ";
+        for (int i = 0; i < graph[u].size(); i++) {
+            int v = graph[u][i];
+            if (!vis[v]) {
+                q.push(v);
+                vis[v] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int node, edge, u, v;
+    cin >> node >> edge;
+    while (edge--) {
+        cin >> u >> v;
+        graph[u].push_back(v);
+    }
+    for (int i = 0; i < node; i++) {
+        if (!vis[i]) {
+            bfs(i);
+        }
+    }
+    cout << "\n";
+    return 0;
+}
