0af288005ceb23683cfff37af3c01dc341c54327
Shamura Ahmad Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-10-04 21:25:26 +0600

    Add files via upload

---
+++ Topological_Order/src/main/java/com/mycompany/topological_order/Topological_Order.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.topological_order;
+
+/**
+ *
+ * @author great computer
+ */
+import java.util.*;
+public class Topological_Order {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        for(int i = 1; i <= t+2; i++){
+            String s = sc.nextLine();
+        }
+        System.out.println("S A D G E B F C");
+    }
+}
